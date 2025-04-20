9d0eb4998fefbc370fa2f8ae37b53839a2c86601
Yago Matias <Yago Matias>
2025-04-20 16:57:46 -0300

    test

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ app.get('/home', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor crosby rodando na porta ${PORT}`);
 });
 
 export default app;
