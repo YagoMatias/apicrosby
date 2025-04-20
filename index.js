57e4fe427cf6859a40a52cb7d607943ba3df6824
Yago Matias <Yago Matias>
2025-04-20 17:58:47 -0300

    install pg

--- index.js
+++ index.js
@@ -123,9 +123,9 @@ app.get('/', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor crosby rodando na porta ${PORT}`);
+const PORT = process.env.port || 3000;
+app.listen(() => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
 
 export default app;
