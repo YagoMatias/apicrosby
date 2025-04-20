303f02e8b6df53ad22c53ce2541e995defe6a187
Yago Matias <Yago Matias>
2025-04-20 16:51:54 -0300

    atualizando rotas

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import pool from './db.js';
 import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 app.use(cors());
@@ -121,6 +123,9 @@ app.get('/home', async (req, res) => {
 //   }
 // });
 
-// app.listen(3000, () => {
-//   console.log('Servidor rodando em http://localhost:3000');
-// });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+export default app;
