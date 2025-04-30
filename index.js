32096ed5638447f3764f9e594ca3f861f3bb5fa3
Yago Matias <Yago Matias>
2025-04-29 23:27:14 -0300

    teste render api

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(cors({ origin: '*' }));
 
 // Rank;
 app.get('/', async (req, res) => {
