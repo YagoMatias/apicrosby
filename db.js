3b6c2840266e6325e639b20fed20366f8e92480b
Yago Matias <yagomatiasdev@gmail.com>
2025-04-30 19:12:15 -0300

    agora vai

--- db.js
+++ db.js
@@ -8,11 +8,6 @@ const pool = new Pool({
   database: process.env.database,
   password: process.env.password,
   port: process.env.port,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  connectionTimeoutMillis: 5000, // Tempo limite de conexão de 5 segundos
-  idleTimeoutMillis: 30000, // Fecha conexões inativas após 30 segundos
 });
 
 export default pool;
