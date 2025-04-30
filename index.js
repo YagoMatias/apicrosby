33299363d45acd7a5345196f14bc61e483815df5
Yago Matias <Yago Matias>
2025-04-30 00:07:38 -0300

    teste

--- index.js
+++ index.js
@@ -196,8 +196,10 @@ app.get('/vendedor', async (req, res) => {
 //   }
 // });
 
-app.listen(3000, () => {
-  console.log(`Servidor rodando na porta http://localhost:3000/`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
 
 export default app;
