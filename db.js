10899f6884163bf9fcf77ad42014023bec3a2a23
Yago Matias <yagomatiasdev@gmail.com>
2025-05-03 10:45:41 -0300

    aaaaaaaaaaaa

--- db.js
+++ db.js
@@ -7,5 +7,11 @@ const pool = new Pool({
   database: process.env.DATABASE,
   password: process.env.PASSWORD,
   port: process.env.PORT,
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 300000,
+    acquire: 300000
+  },
 });
 export default pool;
