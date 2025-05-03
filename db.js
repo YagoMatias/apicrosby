66ba160992500bfcf7de61f9078f15621e13ce5a
Yago Matias <yagomatiasdev@gmail.com>
2025-05-03 12:10:09 -0300

    atualizacao de rotas

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
-import pkg from 'pg';
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
 
-const { Pool } = pkg;
+dotenv.config();
+
+// Conectando ao banco de dados PostgreSQL
 const pool = new Pool({
-  user: process.env.user,
-  host: process.env.host,
-  database: process.env.database,
-  password: process.env.password,
-  port: process.env.port,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },  // Se necessário para a conexão com SSL
 });
+
 export default pool;
