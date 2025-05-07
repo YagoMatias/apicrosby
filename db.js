afe58d9ae97eb17bdc74f81a0b3866884b0aa24c
Yago Matias <yagomatiasdev@gmail.com>
2025-05-07 15:48:39 -0300

    atualizacao

--- db.js
+++ db.js
@@ -1,12 +1,17 @@
-import { Pool } from 'pg';
 import dotenv from 'dotenv';
+import { Pool } from 'pg';
 
+// Carregar as variáveis de ambiente do arquivo .env
 dotenv.config();
 
-// Conectando ao banco de dados PostgreSQL
+// Criação do pool de conexões com o banco de dados
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  user: process.env.user,
+  host: process.env.host,
+  database: process.env.database,
+  password: process.env.password,
+  port: process.env.port,
 });
 
-
+// Exportando o pool para uso em outros módulos
 export default pool;
