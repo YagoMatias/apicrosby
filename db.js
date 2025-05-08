8f81248c5c0d82d1b958f42f6a525f17dd4b478f
Yago Matias <yagomatiasdev@gmail.com>
2025-05-08 12:35:55 -0300

    AGORA VAI

--- db.js
+++ db.js
@@ -6,11 +6,11 @@ dotenv.config();
 
 // Criação do pool de conexões com o banco de dados
 const pool = new Pool({
-  user: process.env.USER,
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: process.env.PORT,
+  user: process.env.user,
+  host: process.env.host,
+  database: process.env.database,
+  password: process.env.password,
+  port: process.env.port,
 });
 
 // Exportando o pool para uso em outros módulos
--- index.js
+++ index.js
@@ -196,7 +196,7 @@ app.get('/health', (req, res) => {
   res.status(200).send('OK');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.port || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor rodando na porta ${PORT}`);
