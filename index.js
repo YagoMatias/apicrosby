f9ad319eeb1d4867ec932e91af875b9016a62f84
Yago Matias <yagomatiasdev@gmail.com>
2025-06-05 10:32:08 -0300

    correção das rotas AS

--- index.js
+++ index.js
@@ -48,12 +48,21 @@ app.get('/', async (req, res) => {
     ) AS PASAIDA,
     COUNT(*) FILTER(WHERE T.TP_SITUACAO = 4 AND T.TP_OPERACAO = 'S') AS TRASAIDA,
     COUNT(*) FILTER(WHERE T.TP_SITUACAO = 4 AND T.TP_OPERACAO = 'E') AS TRAENTRADA,
-    SUM(
-      CASE
-        WHEN T.TP_SITUACAO = 4 AND T.TP_OPERACAO = 'S' THEN T.VL_TOTAL 
-        WHEN T.TP_SITUACAO = 4 AND T.TP_OPERACAO = 'E' THEN -T.VL_TOTAL 
-        ELSE 0
-      END
+    (
+      SUM(
+        CASE
+          WHEN T.TP_SITUACAO = 4 AND T.TP_OPERACAO = 'S' THEN T.VL_TOTAL
+          WHEN T.TP_SITUACAO = 4 AND T.TP_OPERACAO = 'E' THEN -T.VL_TOTAL
+          ELSE 0
+        END
+      )
+      -
+      SUM(
+        CASE
+          WHEN T.TP_SITUACAO = 4 AND T.TP_OPERACAO IN ('S', 'E') THEN COALESCE(T.VL_FRETE, 0)
+          ELSE 0
+        END
+      )
     ) AS FATURAMENTO
 FROM
     GER_EMPRESA A
@@ -61,52 +70,28 @@ JOIN
     PES_PESJURIDICA B ON A.CD_PESSOA = B.CD_PESSOA
 LEFT JOIN
     TRA_TRANSACAO T ON T.CD_GRUPOEMPRESA = A.CD_GRUPOEMPRESA
-WHERE B.CD_PESSOA < 99999 AND B.CD_PESSOA NOT IN(69994,70596) AND
-T.VL_TOTAL > 1 AND
-    (
-        T.TP_SITUACAO IS NULL OR
-        (
-            T.TP_SITUACAO = 4 AND
-            T.TP_OPERACAO IN ('S', 'E') AND
-            T.CD_OPERACAO IN (1,2,510,511,1511,521,1521,522,960,9001,9009,9027,8750,9017,9400,9401,9402,9403,9005,545,546,555,548,1210,9404,9405) AND
-            T.CD_GRUPOEMPRESA BETWEEN $1 AND $2 AND
-            T.DT_TRANSACAO BETWEEN $3::timestamp AND $4::timestamp
+WHERE 
+    B.CD_PESSOA NOT IN (69994,70596,110000001,73469,61000007,61000008,61000009,61000010)
+    AND B.CD_PESSOA < 110000100
+    AND T.VL_TOTAL > 1
+    AND (
+        T.TP_SITUACAO IS NULL OR (
+            T.TP_SITUACAO = 4
+            AND T.TP_OPERACAO IN ('S', 'E')
+            AND T.CD_OPERACAO IN (1,2,510,511,1511,521,1521,522,960,9001,9009,9027,8750,9017,9400,9401,9402,9403,9404,9005,545,546,555,548,1210,9405,1205)
+            AND T.CD_GRUPOEMPRESA BETWEEN $1 AND $2
+            AND T.DT_TRANSACAO BETWEEN $3::timestamp AND $4::timestamp
         )
     )
-    -
-    SUM(
-      CASE
-        WHEN T.TP_SITUACAO = 4 AND T.TP_OPERACAO IN ('S', 'E') THEN COALESCE(T.VL_FRETE, 0)
-        ELSE 0
-      END)
-      
-    AS FATURAMENTO
-    FROM
-        GER_EMPRESA A
-    JOIN
-        PES_PESJURIDICA B ON A.CD_PESSOA = B.CD_PESSOA
-    LEFT JOIN
-        TRA_TRANSACAO T ON T.CD_GRUPOEMPRESA = A.CD_GRUPOEMPRESA
-    WHERE B.CD_PESSOA < 110000006 AND B.CD_PESSOA NOT IN(69994,70596,110000001,73469,61000007,61000008,61000009,61000010)     AND
-    T.VL_TOTAL > 1 AND
-        (
-            T.TP_SITUACAO IS NULL OR
-            (
-                T.TP_SITUACAO = 4 AND
-                T.TP_OPERACAO IN ('S', 'E') AND
-                T.CD_OPERACAO IN (1,2,510,511,1511,521,1521,522,960,9001,9009,9027,8750,9017,9400,9401,9402,9403,9404,9005,545,546,555,548,1210,9405,1205) AND
-                T.CD_GRUPOEMPRESA BETWEEN $1 AND $2 AND
-                T.DT_TRANSACAO BETWEEN $3::timestamp AND $4::timestamp
-            )
-        )
-    GROUP BY
-        A.CD_GRUPOEMPRESA,
-        A.CD_PESSOA,
-        B.CD_PESSOA,
-        B.NM_FANTASIA
-    ORDER BY
+GROUP BY
+    A.CD_GRUPOEMPRESA,
+    A.CD_PESSOA,
+    B.CD_PESSOA,
+    B.NM_FANTASIA
+ORDER BY
     FATURAMENTO DESC,
-    B.NM_FANTASIA`,
+    B.NM_FANTASIA;
+`,
       [1, 7000, [dataInicio], [dataFim]],
     );
 
@@ -128,43 +113,50 @@ app.get('/home', async (req, res) => {
   try {
     const resultado = await pool.query(
       `SELECT
-          SUM(
-          CASE
-            WHEN TP_OPERACAO = 'E' AND TP_SITUACAO = 4 THEN QT_SOLICITADA
-            ELSE 0
-           END
-          ) AS PAENTRADA,
-          SUM(
-          CASE
-            WHEN TP_OPERACAO = 'S' AND TP_SITUACAO = 4 THEN QT_SOLICITADA
-            ELSE 0
-           END
-          ) AS PASAIDA,
-          COUNT(*) FILTER(WHERE TP_SITUACAO = 4 AND TP_OPERACAO = 'S') AS TRASAIDA,
-          COUNT(*) FILTER(WHERE TP_SITUACAO = 4 AND TP_OPERACAO = 'E') AS TRAENTRADA,
-          SUM(
-             CASE
-               WHEN TP_SITUACAO = 4 AND TP_OPERACAO = 'S' THEN VL_TOTAL
-               WHEN TP_SITUACAO = 4 AND TP_OPERACAO = 'E' THEN -VL_TOTAL
-               ELSE 0
-             END) -
+  SUM(
+    CASE
+      WHEN TP_OPERACAO = 'E' AND TP_SITUACAO = 4 THEN QT_SOLICITADA
+      ELSE 0
+    END
+  ) AS PAENTRADA,
+  SUM(
+    CASE
+      WHEN TP_OPERACAO = 'S' AND TP_SITUACAO = 4 THEN QT_SOLICITADA
+      ELSE 0
+    END
+  ) AS PASAIDA,
+  COUNT(*) FILTER (WHERE TP_SITUACAO = 4 AND TP_OPERACAO = 'S') AS TRASAIDA,
+  COUNT(*) FILTER (WHERE TP_SITUACAO = 4 AND TP_OPERACAO = 'E') AS TRAENTRADA,
+  (
+    SUM(
+      CASE
+        WHEN TP_SITUACAO = 4 AND TP_OPERACAO = 'S' THEN VL_TOTAL
+        WHEN TP_SITUACAO = 4 AND TP_OPERACAO = 'E' THEN -VL_TOTAL
+        ELSE 0
+      END
+    )
+    -
     SUM(
       CASE
         WHEN TP_SITUACAO = 4 AND TP_OPERACAO IN ('S', 'E') THEN COALESCE(VL_FRETE, 0)
         ELSE 0
-      END)
-      
-    AS FATURAMENTO
-         FROM TRA_TRANSACAO
-         WHERE
-           TP_SITUACAO = 4 AND
-           TP_OPERACAO IN ('S', 'E') AND
-           CD_OPERACAO IN (1,2,510,511,1511,521,1521,522,960,9001,9009,9027,8750,9017,9400,9401,9402,9403,9005,545,546,555,548,1210,9404,9405,1205) AND
-           DT_TRANSACAO BETWEEN $1::timestamp AND $2::timestamp
-         ORDER BY FATURAMENTO DESC`,
+      END
+    )
+  ) AS FATURAMENTO
+FROM TRA_TRANSACAO
+WHERE
+  TP_SITUACAO = 4
+  AND TP_OPERACAO IN ('S', 'E')
+  AND CD_OPERACAO IN (
+    1,2,510,511,1511,521,1521,522,960,
+    9001,9009,9027,8750,9017,9400,9401,
+    9402,9403,9005,545,546,555,548,1210,
+    9404,9405,1205
+  )
+  AND DT_TRANSACAO BETWEEN $1::timestamp AND $2::timestamp;
+`,
       [[dataInicio], [dataFim]],
     );
-
     res.json(resultado.rows);
     console.log(resultado.rows);
   } catch (err) {
@@ -181,45 +173,53 @@ app.get('/vendedor', async (req, res) => {
   try {
     const resultado = await pool.query(
       `SELECT
-       A.CD_VENDEDOR AS VENDEDOR,
-       A.NM_VENDEDOR AS NOME_VENDEDOR,
-       B.CD_COMPVEND,
-          SUM(
-      CASE
-        WHEN B.TP_OPERACAO = 'E' AND B.TP_SITUACAO = 4 THEN B.QT_SOLICITADA
-        ELSE 0
-      END
-    ) AS PAENTRADA,
+  A.CD_VENDEDOR AS VENDEDOR,
+  A.NM_VENDEDOR AS NOME_VENDEDOR,
+  B.CD_COMPVEND,
+  SUM(
+    CASE
+      WHEN B.TP_OPERACAO = 'E' AND B.TP_SITUACAO = 4 THEN B.QT_SOLICITADA
+      ELSE 0
+    END
+  ) AS PAENTRADA,
+  SUM(
+    CASE
+      WHEN B.TP_OPERACAO = 'S' AND B.TP_SITUACAO = 4 THEN B.QT_SOLICITADA
+      ELSE 0
+    END
+  ) AS PASAIDA,
+  COUNT(*) FILTER (WHERE B.TP_SITUACAO = 4 AND B.TP_OPERACAO = 'S') AS TRASAIDA,
+  COUNT(*) FILTER (WHERE B.TP_SITUACAO = 4 AND B.TP_OPERACAO = 'E') AS TRAENTRADA,
+  (
     SUM(
       CASE
-        WHEN B.TP_OPERACAO = 'S' AND B.TP_SITUACAO = 4 THEN B.QT_SOLICITADA
-        ELSE 0
-      END
-    ) AS PASAIDA,
-    COUNT(*) FILTER(WHERE B.TP_SITUACAO = 4 AND B.TP_OPERACAO = 'S') AS TRASAIDA,
-    COUNT(*) FILTER(WHERE B.TP_SITUACAO = 4 AND B.TP_OPERACAO = 'E') AS TRAENTRADA,
-    SUM(
-      CASE
-        WHEN B.TP_SITUACAO = 4 AND B.TP_OPERACAO = 'S' THEN B.VL_TOTAl
+        WHEN B.TP_SITUACAO = 4 AND B.TP_OPERACAO = 'S' THEN B.VL_TOTAL
         WHEN B.TP_SITUACAO = 4 AND B.TP_OPERACAO = 'E' THEN -B.VL_TOTAL
         ELSE 0
       END
-    )  -
+    )
+    -
     SUM(
       CASE
         WHEN B.TP_SITUACAO = 4 AND B.TP_OPERACAO IN ('S', 'E') THEN COALESCE(B.VL_FRETE, 0)
         ELSE 0
-      END)
-      
-    AS FATURAMENTO
-      FROM PES_VENDEDOR A
-      JOIN TRA_TRANSACAO B ON A.CD_VENDEDOR = B.CD_COMPVEND
-      WHERE B.TP_SITUACAO = 4
-        AND B.TP_OPERACAO IN ('S', 'E')
-        AND B.CD_OPERACAO IN (1,2,510,511,1511,521,1521,522,960,9001,9009,9027,8750,9017,9400,9401,9402,9403,9005,545,546,555,548,1210,1202,8800,9404,9405,1205)
-        AND B.DT_TRANSACAO BETWEEN $1::timestamp AND $2::timestamp
-      GROUP BY A.CD_VENDEDOR, A.NM_VENDEDOR, B.CD_COMPVEND
-      ORDER BY FATURAMENTO DESC`,
+      END
+    )
+  ) AS FATURAMENTO
+FROM PES_VENDEDOR A
+JOIN TRA_TRANSACAO B ON A.CD_VENDEDOR = B.CD_COMPVEND
+WHERE B.TP_SITUACAO = 4
+  AND B.TP_OPERACAO IN ('S', 'E')
+  AND B.CD_OPERACAO IN (
+    1,2,510,511,1511,521,1521,522,960,
+    9001,9009,9027,8750,9017,9400,9401,
+    9402,9403,9005,545,546,555,548,1210,
+    1202,8800,9404,9405,1205
+  )
+  AND B.DT_TRANSACAO BETWEEN $1::timestamp AND $2::timestamp
+GROUP BY A.CD_VENDEDOR, A.NM_VENDEDOR, B.CD_COMPVEND
+ORDER BY FATURAMENTO DESC;
+`,
       [[dataInicio], [dataFim]],
     );
     res.json(resultado.rows);
@@ -235,40 +235,35 @@ app.get('/estoque', async (req,res)=>{
     try {
     const resultado = await pool.query(
   `SELECT
-       A.cd_grupoempresa,
-       PJ.nm_fantasia as nome,
-       SUM(A.qt_saldo) AS saldo,
-       SUM(V.vl_produto * A.qt_saldo) AS valor
-     FROM
-       vr_prd_saldo A
-     JOIN (
-       SELECT *
-       FROM (
-         SELECT *,
+        A.cd_grupoempresa,
+        PJ.nm_fantasia AS nome,
+        SUM(A.qt_saldo) AS saldo,
+        SUM(V.vl_produto * A.qt_saldo) AS valor
+      FROM vr_prd_saldo A
+      JOIN (
+        SELECT *
+        FROM (
+          SELECT *,
                 ROW_NUMBER() OVER (PARTITION BY cd_produto ORDER BY dt_cadastro DESC) AS rn
-         FROM vr_prd_valorprod
-         WHERE ds_valor = 'VENDA VAREJO'
-       ) sub
-       WHERE rn = 1
-     ) V ON A.cd_produto = V.cd_produto
-     JOIN ger_empresa E ON A.cd_grupoempresa = E.cd_grupoempresa
-     JOIN pes_pesjuridica PJ ON E.cd_pessoa = PJ.cd_pessoa
-     WHERE
-       A.cd_grupoempresa >= 6000
-       AND A.qt_saldo != 0
-       AND PJ.nm_fantasia IS NOT NULL
-       AND PJ.nm_fantasia NOT ILIKE '%TESTE%' -- ignora qualquer nome que comece com "TESTE"
-       AND A.cd_produto NOT IN (
-         29406, 29387, 29647, 32570, 32571, 32691, 29609, 29610, 29646,
-         39413, 34538, 4538, 34710, 29607, 29648, 39897,
-         29400, 44879, 5000033, 44725, 49084, 38071,
-         29397, 5000015, 64568, 64570
-       )
-     GROUP BY
-       A.cd_grupoempresa,
-       PJ.nm_fantasia
-     ORDER BY
-       A.cd_grupoempresa ASC`,
+          FROM vr_prd_valorprod
+          WHERE ds_valor = 'VENDA VAREJO'
+        ) sub
+        WHERE rn = 1
+      ) V ON A.cd_produto = V.cd_produto
+      JOIN ger_empresa E ON A.cd_grupoempresa = E.cd_grupoempresa
+      JOIN pes_pesjuridica PJ ON E.cd_pessoa = PJ.cd_pessoa
+      WHERE
+        A.cd_grupoempresa >= 6000 AND
+        A.qt_saldo != 0 AND
+        PJ.nm_fantasia IS NOT NULL AND
+        PJ.nm_fantasia NOT ILIKE '%TESTE%' AND
+        A.cd_produto NOT IN (
+          29406, 29387, 29647, 32570, 32571, 32691, 29609, 29610, 29646,
+          39413, 34538, 4538, 34710, 29607, 29648, 39897, 29400, 44879,
+          5000033, 44725, 49084, 38071, 29397, 5000015, 64568, 64570
+        )
+      GROUP BY A.cd_grupoempresa, PJ.nm_fantasia
+      ORDER BY A.cd_grupoempresa ASC`,
     );
     console.log('Estoque rodando');
     res.json(resultado.rows);
@@ -330,7 +325,7 @@ app.get('/health', (req, res) => {
 const PORT = process.env.port || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT});
 });
 
 export default app;
