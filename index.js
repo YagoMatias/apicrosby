158922ef0992ff4b3e089f24966e08bcb38487a8
Yago Matias <yagomatiasdev@gmail.com>
2025-06-16 17:07:51 -0300

    atualizacao de senha db

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ JOIN
 LEFT JOIN
     TRA_TRANSACAO T ON T.CD_GRUPOEMPRESA = A.CD_GRUPOEMPRESA
 WHERE 
-    B.CD_PESSOA NOT IN (69994,70596,110000001,73469,61000007,61000008,61000009,61000010)
+    B.CD_PESSOA NOT IN (69994,70596,110000001,73469,61000007,61000008,61000009,61000010,45832)
     AND B.CD_PESSOA < 110000100
     AND T.VL_TOTAL > 1
     AND (
@@ -102,7 +102,6 @@ ORDER BY
     res.status(500).send('deu erro nessa desgrama');
   }
 });
-
 // Home;
 app.get('/home', async (req, res) => {
   const inicio = req.query.inicio;
@@ -164,6 +163,17 @@ WHERE
     res.status(500).send('Erro no servidor');
   }
 });
+
+app.get('/empresas', async (req, res) => {
+  try {
+    const resultado = await pool.query('SELECT cd_grupoempresa, nm_grupoempresa FROM vr_ger_empresa where cd_grupoempresa > 5999 AND cd_empresa % 2 = 0 order by cd_grupoempresa asc');
+    res.json(resultado.rows);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Erro ao buscar empresas');
+  }
+});
+
 //Vendedor
 app.get('/vendedor', async (req, res) => {
   const inicio = req.query.inicio;
@@ -230,6 +240,59 @@ ORDER BY FATURAMENTO DESC;
   }
 });
 
+//Curvaabc
+app.get('/curvaabc', async (req, res) => {
+  const inicio = req.query.inicio;
+  const fim = req.query.fim;
+  const cdGrupoempresa = req.query.cd_grupoempresa;
+  const dataInicio = `${inicio} 00:00:00`;
+  const dataFim = `${fim} 23:59:59`;
+
+  try {
+    const resultado = await pool.query(
+      `SELECT 
+        A.cd_grupoempresa as gpEmpresa,
+        A.CD_PRODUTO as cdProduto,
+        A.ds_produto as nmProduto, 
+        vp.cd_seqgrupo as grupo,
+        vp.ds_cor as cor,
+        vp.ds_tamanho as tamanho,
+        A.vl_totalliquido as valorVendido,
+        pj.nm_grupoempresa as nmEmpresa,
+        vp.ds_nivel as nmGrupo
+      FROM fis_nfitem A
+      JOIN vr_produtos vp 
+        ON CAST(A.CD_PRODUTO AS TEXT) = CAST(vp.CD_PRODUTO AS TEXT)
+      LEFT JOIN vr_ger_empresa pj on a.cd_grupoempresa = pj.cd_grupoempresa
+      WHERE 
+        A.cd_grupoempresa = $3
+        AND a.DT_fatura BETWEEN $1::timestamp AND $2::timestamp
+        AND CAST(a.cd_produto AS INTEGER) NOT IN (
+          29406, 29387, 29647, 32570, 32571, 32691, 29609, 29610, 29646,
+          39413, 34538, 4538, 34710, 29607, 29648, 39897, 29400, 44879,
+          5000033, 44725, 49084, 38071, 29397, 5000015, 64568, 64570
+        )
+      GROUP BY 
+        A.cd_grupoempresa,
+        A.cd_produto,
+        A.ds_produto,
+        A.vl_totalliquido, 
+        vP.cd_seqgrupo,
+        vp.ds_cor,  
+        vp.ds_tamanho,
+        pj.nm_grupoempresa,
+        vp.ds_nivel
+      ORDER BY A.vl_totalliquido DESC`,
+      [dataInicio, dataFim, cdGrupoempresa]  // Passando o parâmetro corretamente
+    );
+
+    res.json(resultado.rows); // Retornando os dados da consulta
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Erro no servidor');
+  }
+});
+
 //Estoque
 app.get('/estoque', async (req,res)=>{
     try {
@@ -263,6 +326,52 @@ app.get('/estoque', async (req,res)=>{
           5000033, 44725, 49084, 38071, 29397, 5000015, 64568, 64570
         )
       GROUP BY A.cd_grupoempresa, PJ.nm_fantasia
+      ORDER BY A.cd_grupoempresa ASC`,
+    );
+    console.log('Estoque rodando');
+    res.json(resultado.rows);
+    console.log(resultado.rows);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send(`erro no servidor`);
+  }
+})
+
+//estoqueporempresa
+app.get('/estoqueporempresa', async (req,res)=>{
+    try {
+    const resultado = await pool.query(
+  `SELECT
+        A.cd_grupoempresa,
+        PJ.nm_fantasia AS nome,
+        a.cd_produto as CodPRD,
+        A.ds_produto as Descrição,
+        SUM(A.qt_saldo) AS saldo,
+        SUM(V.vl_produto * A.qt_saldo) AS valor
+      FROM vr_prd_saldo A
+      JOIN (
+        SELECT *
+        FROM (
+          SELECT *,
+                ROW_NUMBER() OVER (PARTITION BY cd_produto ORDER BY dt_cadastro DESC) AS rn
+          FROM vr_prd_valorprod
+          WHERE ds_valor = 'VENDA VAREJO'
+        ) sub
+        WHERE rn = 1
+      ) V ON A.cd_produto = V.cd_produto
+      JOIN ger_empresa E ON A.cd_grupoempresa = E.cd_grupoempresa
+      JOIN pes_pesjuridica PJ ON E.cd_pessoa = PJ.cd_pessoa
+      WHERE
+        A.cd_grupoempresa = 6000 AND
+        A.qt_saldo != 0 AND
+        PJ.nm_fantasia IS NOT NULL AND
+        PJ.nm_fantasia NOT ILIKE '%TESTE%' AND
+        A.cd_produto NOT IN (
+          29406, 29387, 29647, 32570, 32571, 32691, 29609, 29610, 29646,
+          39413, 34538, 4538, 34710, 29607, 29648, 39897, 29400, 44879,
+          5000033, 44725, 49084, 38071, 29397, 5000015, 64568, 64570
+        )
+      GROUP BY A.cd_grupoempresa, PJ.nm_fantasia,a.cd_produto,A.ds_produto
       ORDER BY A.cd_grupoempresa ASC`,
     );
     console.log('Estoque rodando');
@@ -324,8 +433,8 @@ app.get('/health', (req, res) => {
 
 const PORT = process.env.port || 3000;
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+app.listen(3000, '0.0.0.0', () => {
+  console.log(`Servidor rodando na porta http://localhost:3000/`);
 });
 
 export default app;
